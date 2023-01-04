c9aa2e0a255fa9de1883a034b1b92680f0f71cee
Chris Sutcliffe <cjsutcliffe@gmail.com>
2023-01-04 14:44:00 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Modal from "./modal"
+import './App.css'
 
-function App() {
+const App = () => {
+  const [errorMsg, setErrorMsg] = useState('');
+  const [currentCountry, setCurrentCountry] = useState({});
+  const [countries, setCountries] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setErrorMsg('')
+        const response = await fetch('https://restcountries.com/v3.1/all/');
+        
+        if(!response.ok){
+          throw new Error(response.statusText)
+        }
+
+        const data = await response.json();
+        //sorts api data alphabettically by country common name and sets this as the countries
+        setCountries(data.sort((a, b) => a.name.common.localeCompare(b.name.common)))
+      } catch (error) {
+        setErrorMsg("Well this is embarasing, something went wrong...")
+        console.log(error.message)
+      }
+    }
+    fetchData()
+  }, [])
+
+  
+  const handleClick = (country) => {
+    setCurrentCountry(country);
+    setShowModal(true)
+  }  
+
+  if(errorMsg !== ''){
+    return <h1>{errorMsg}</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pageWrapper">
+
+      <h1>Countries of the World</h1>
+
+   
+      
+      {/* map through data from API that is stored in the state */}
+      
+      <div className="imageWrapper">
+        {countries
+          // .sort((a, b) => a.country.name.commonname - b.country.name.common)
+          .map((country, index) => {
+          return (
+            <div className="countryGrid">
+            <h2 onClick={() => handleClick(country)}>{country.name.common}</h2>
+            <img key={index} src={country.flags.png} alt={country.name.common} onClick={() => handleClick(country)} width="10%" />
+            
+            </div>
+          )
+        })}
+            
+
+            <div className="footer">
+            Chris Sutcliffe <></>
+            <br></br>
+            <a onclick href="https://github.com/cjsutcliffe/cn-react-api-challenge">GitHub</a>
+            <br></br>
+            <code onclick href="https://restcountries.com/v3.1/all/">API endpoint: https://restcountries.com/v3.1/all/</code>
+            </div>
+
+      </div>
+
+
+
+      {console.log(currentCountry)}
+      {showModal && <Modal closeModal={setShowModal} country={currentCountry} />}
     </div>
   );
 }
---
+++ src/modal.js
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+
+const Modal = (props) => {
+    const modalRef = useRef();
+
+    const handleClick = (e) => {
+        if(modalRef.current === e.target){
+            props.closeModal(false)
+        }
+    }
+    const handleCloseClick = (e) => {
+        props.closeModal(false)
+    }
+
+    return (
+        <div className="modalBackground" ref={modalRef} onClick={handleClick}>
+            <div className="modalBox">
+                <p className="closeIcon" onClick={handleCloseClick}>x</p>
+                <div className="topSection">
+                    <img src={props.country.flags.png} width="30px" alt={`Country Flag`} />
+                    <img src={props.country.coatOfArms.png} width="30px" alt={`Country Flag`} />
+                    <div className="whiteGradient"></div>
+                </div>
+                <div className="bottomSection">                    
+                    <h1>{props.country.name.common}</h1>                    
+                    <p>Official Name: {props.country.name.official}</p>
+                    <p>Capital City: {props.country.capital}</p>
+                    <p>Population: {props.country.population}</p>
+                    <p>In {props.country.name.common} they drive on the {props.country.car.side} hand side of the road</p>
+                    <a style={{display: "mapButton"}} href={props.country.maps.googleMaps} target="_blank" rel="noreferrer"> Click for map</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
